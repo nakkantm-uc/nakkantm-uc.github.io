1a11a294eb4dddd644c4ef969f9a8b8fd633fc96
Tulasiram Nakkanaboina <155696605+nakkantm-uc@users.noreply.github.com>
2024-02-14 12:52:27 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
+window.addEventListener('DOMContentLoaded', () => {
+    // Place all your code here
     // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
@@ -30,5 +21,4 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
 });
