74ac8f096af5123816a71076bf5d0c15fb2d7947
Tulasiram Nakkanaboina <155696605+nakkantm-uc@users.noreply.github.com>
2024-02-14 20:37:25 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,25 @@
-// Function to activate Bootstrap scrollspy on the main nav element
-function activateScrollSpy() {
-    const sideNav = document.querySelector('#sideNav');
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#sideNav',
-            rootMargin: '0px 0px 0px 0px', // Adjust this value as needed
-});
-
+            rootMargin: '0px 0px -40%',
         });
-    }
-}
+    };
 
-// Function to collapse responsive navbar when toggler is visible
-function collapseNavbar() {
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    const responsiveNavItems = document.querySelectorAll('#navbarResponsive .nav-link');
-    responsiveNavItems.forEach(responsiveNavItem => {
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
             }
         });
     });
-}
 
-// Function to execute when DOM content is loaded
-function init() {
-    activateScrollSpy();
-    collapseNavbar();
-}
-
-// Event listener for DOMContentLoaded event
-document.addEventListener('DOMContentLoaded', init);
+});
